705c310da671628865cc45a7d4a9d3667bd572fd
Daniel Szukitsch <daniel@szuuuken.at>
2017-05-14 19:00:46 +0200

    first steps

--- avatar/gulpfile.js
+++ avatar/gulpfile.js
@@ -7,19 +7,19 @@ var sourcemaps = require('gulp-sourcemaps');
 var bom = require('gulp-bom');
 
 gulp.task('sass', function () {
-  return gulp.src('./avatar.sass')
+  return gulp.src('./app/*.sass')
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./dist/'))
     .pipe(reload({stream: true}));
 });
 
 gulp.task('templates', function () {
-  return gulp.src('./avatar.jade')
+  return gulp.src('./app/*.jade')
     .pipe(jade())
     .pipe(bom())
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./dist/'))
     .pipe(reload({stream: true}));
 });
 
@@ -28,7 +28,7 @@ gulp.task('jade-watch', ['templates']);
 gulp.task('build', ['sass', 'templates']);
 
 gulp.task('default', ['build'], function () {
-  browserSync.init({server: './avatar.html'});
-  gulp.watch('.avatar.jade', ['jade-watch']);
-  gulp.watch('./avatar.sass', ['sass']);
+  browserSync.init({server: './dist/'});
+  gulp.watch('./app/*.jade', ['jade-watch']);
+  gulp.watch('./app/*.sass', ['sass']);
 });
